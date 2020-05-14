18beba826794050dc0025414c4ada9adc9a5bd56
Robert Lopez <robert.lopez@yale.edu>
2020-05-14 01:01:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,6 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
+  programmer_hash[:grace_hopper]  
 
 end
 
@@ -36,6 +37,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+    programmer_hash[:alan_kay][:known_for]
 
 end
 
@@ -55,6 +57,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+    programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
@@ -81,7 +84,12 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:yukihiro_matsumoto] = {
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
+    }
+    
+puts programmer_hash
 end
 
 def changing_alan
@@ -125,5 +133,4 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
 end
